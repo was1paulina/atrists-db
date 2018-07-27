9f773ef2c5453a7dff43678d575e099cecb5332a
was1paulina <palina@Palinas-MBP-2.sea.centerid.net>
2018-07-27 12:02:29 -0700

    Adding Artist page

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 
-import Banner from '../components/banner';
+import Banner from './banner';
+import ArtistsList from './artistsList';
+
+const URL_ARTIST = 'http://localhost:3005/artists'
 
 class Home extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			artists:''
+		}
+	}
+
+	componentDidMount() {
+		fetch(URL_ARTIST, {
+			method: 'GET'
+		})
+		.then(response => response.json())
+		.then(json => {
+			this.setState({artists:json})
+		})
+	}
+
 	render(){
 		return(
 			<div>
-				<Banner></Banner>
+				<Banner /> 
+				<ArtistsList allArtists={this.state.artists}/> 
 			</div>
 			)
 	}
